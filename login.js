6aceb657ef1b44c5a40b73bf9c46c3765ca1704d
RoshiniR17 <2017-18ece31@nec.edu.in>
2020-06-28 00:05:29 +0530

    workinn!!!

--- login.js
+++ login.js
@@ -10,14 +10,16 @@ module.exports = function(app,mongoDbUrl, MongoClient){
             var db = allDb.db('covidcare');//db name
 
             db.collection("users").find(userLoginInput).toArray(function(err, result) {
+            console.log(result[0]);
                 if (err){
                     console.log(err);
                     res.send(JSON.stringify("false user"));
-                }else if (result[0] === undefined){
+                }else if (result[0]===undefined ){
+
                     console.log("false user");
                     res.send(JSON.stringify("false user"));
                 }else{
-                    console.log(result);
+                    console.log(userLoginInput,"success");
                     res.send(JSON.stringify("true user"));
                 };
                 allDb.close();
